6aa7868810557e1d1eee33b9985f8eff4cb466df
Jorge Santos <neneluis55@gmail.com>
2019-12-17 05:37:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
 # Add your variables here
-first_number = calculator.rb
+expect(first_number).to be_an(Integer).or be_a(Float)
 expect(second_number).not_to equal(0)
 sum = (first_number + second_number)
 difference = (first_number - second_number)
