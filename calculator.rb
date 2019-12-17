64c06bc3e303e1311bbbaa665a28574e539e35bf
Jorge Santos <neneluis55@gmail.com>
2019-12-17 05:42:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-frist_number= '../calculator.rb'
-second_number= '../calculator.rb'
+frist_number = '../calculator.rb'
+second_number = '../calculator.rb'
 sum = (first_number + second_number)
 difference = (first_number - second_number)
 product = (first_number * second_number)
