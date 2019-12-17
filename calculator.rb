21484b7a73fc9961b7b038e395d3507617f3e7d8
Jorge Santos <neneluis55@gmail.com>
2019-12-17 05:39:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-expect(first_number).to be_an(Integer).or be_a(Float)
-expect(second_number).not_to equal(0)
+frist_number='./calculator.rb'
+second_number='./calculator.rb'
 sum = (first_number + second_number)
 difference = (first_number - second_number)
 product = (first_number * second_number)
