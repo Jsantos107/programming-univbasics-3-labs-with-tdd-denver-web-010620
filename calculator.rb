f8bc29bda3740d09bc03ae6566d77b7fbc8ca379
Jorge Santos <neneluis55@gmail.com>
2019-12-17 05:44:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number = 1
-second_number = 5
+first_number = 7
+second_number = 2
 sum = (first_number + second_number)
 difference = (first_number - second_number)
 product = (first_number * second_number)
