e7b5bdb96c3a59f623b5d694224e826f6e50e8cc
Jorge Santos <neneluis55@gmail.com>
2019-12-17 05:36:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 first_number = calxulator.rb
-second_number = 15
+expect(second_number).not_to equal(0)
 sum = (first_number + second_number)
 difference = (first_number - second_number)
 product = (first_number * second_number)
